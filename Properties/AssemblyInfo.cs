dd9a8638313d4b59fc47be9c75e4570c54581bc9
aygjiay <aygjiay@gmail.com>
2016-10-20 17:50:52 -0500

    First AutoMapper.DataReaderMapper version published

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoMapper.DataReaderMapper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoMapper.DataReaderMapper")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18370bc7-2a40-4e12-9875-88beb88458fa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
---
+++ ReflectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace AutoMapper.DataReaderMapper
+{
+   using System;
+
+   internal static class ReflectionExtensions
+   {
+      public static bool IsEnum(this Type type)
+      {
+#if DOTNET
+            return type.GetTypeInfo().IsEnum;
+#else
+         return type.IsEnum;
+#endif
+      }
+
+      public static bool IsGenericType(this Type type)
+      {
+#if DOTNET
+            return type.GetTypeInfo().IsGenericType;
+#else
+         return type.IsGenericType;
+#endif
+      }
+
+      public static bool IsValueType(this Type type)
+      {
+#if DOTNET
+            return type.GetTypeInfo().IsValueType;
+#else
+         return type.IsValueType;
+#endif
+      }
+   }
+}
